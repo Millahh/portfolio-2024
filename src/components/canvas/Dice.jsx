8ffa8dfea9732ff2da28e080a94b91e6306c2d89
millahh <millahmillah37@gmail.com>
2024-07-11 01:34:45 +0700

    ad useAnimate to handle asynchronous operations

--- src/components/canvas/Dice.jsx
+++ src/components/canvas/Dice.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 import { useGLTF } from "@react-three/drei";
-import { motion } from "framer-motion-3d";
 
-const Dice = ({ position, rotation, isThrowed }) => {
+const Dice = ({ position, rotation}) => {
     const dice = useGLTF('./dice/scene.gltf')
 
     return (
-        <motion.mesh position={position}
-          initial={{ scale: 1 }}
-          animate={{ scale : isThrowed ? 0.4 : 1 }}
-        >
+        <mesh position={position}>
             {/* <directionalLight position={[30, 0, -200]} intensity={7}/> */}
             <primitive 
                 object={dice.scene} 
@@ -18,7 +14,7 @@ const Dice = ({ position, rotation, isThrowed }) => {
                 position={[0, 0, 0]} 
                 rotation={rotation}
                 /> 
-        </motion.mesh>
+        </mesh>
     )
 }
 
--- src/components/canvas/DiceIntro.jsx
+++ src/components/canvas/DiceIntro.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect, Suspense } from 'react';
 import { Canvas } from "@react-three/fiber";
 import { Physics, RigidBody } from "@react-three/rapier";
 import { Box, OrbitControls} from "@react-three/drei";
+import { useAnimate, usePresence } from "framer-motion";
 import Dice from "./Dice";
 import CanvasLoader from '../LoaderProgressBar';
 
@@ -15,14 +16,18 @@ const Bg = () => {
 }
 
 const ThrowDice = () => {
-    const [isThrowed, setThrowed] = useState(false);
+    const [scope, animate] = useAnimate();
 
     useEffect(() => {
         // Use setTimeout to update the message after 2000 milliseconds (2 seconds)
         const timeoutId = setTimeout(() => {
-          setThrowed(true);
-        }, 2700);
-    
+            const handleAnimate = async () => {
+                await animate(scope.current, {scale : 0.4})
+                await animate(scope.current, {x:100})
+
+            }
+            handleAnimate()    
+        }, 3200);
         // Cleanup function to clear the timeout if the component unmounts
         return () => clearTimeout(timeoutId);
     }, []);
@@ -30,17 +35,18 @@ const ThrowDice = () => {
     return(
         <div className=" h-screen w-screen bg-custom bg-cover">
             <Canvas 
-                frameloop="demand"
+                frameloop="always"
                 shadows
                 camera={{ position: [200, 40, 50], fov: 7.5}}
                 gl={{ preserveDrawingBuffer: true}}
+                ref={scope} 
                 >
-                <Suspense fallback={<CanvasLoader/>}>
+                <Suspense fallback={<CanvasLoader all/>}>
                     <Physics>
                         <OrbitControls enableZoom={false} enableRotate={false}/>
                         <directionalLight position={[100, 10, 200]} intensity={6}/>
                         <RigidBody gravityScale={10}>
-                            <Dice position={[30, 17, 20]} rotation={[1, -5, 4]} isThrowed={isThrowed}/>
+                            <Dice position={[30, 17, 20]} rotation={[1, -5, 4]}/>
                         </RigidBody>
                         <RigidBody type="fixed">
                             <Box position={[-10,1,-5]} args={[500,0.1,500]}>
