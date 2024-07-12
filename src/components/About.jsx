0462e5f374eba9d83dc0dcf06ef3caae2cd4fd0d
millahh <millahmillah37@gmail.com>
2024-07-12 23:58:46 +0700

    about section -edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import T from "./texts/Title";
 import { AdminLTE, Bootstrap, CSS, Figma, HTML, JavaScript, Laravel, PHP, phpMyAdmin, PostgreSQL, React, Tailwind, Threejs, Github } from "../assets";
 import { motion } from "framer-motion";
@@ -47,6 +48,16 @@ const HardSkills = () => {
     )
 }
 
+const Logos = ({ logos }) => {
+    return (
+        <div className="inline">
+            {logos.map((logos) => (
+                <img key={logos.logo} src={logos.logo} width={logos.width} className="inline"/>
+            ))}
+        </div>
+    )
+}
+
 const About = () => {
     return(
         <div className="container pl-10 min-w-full h-full">
@@ -55,31 +66,15 @@ const About = () => {
                 <div className=" basis-2/5"></div>
                 <div className=" basis-3/5 py-10 px-10 h-screen overflow-y-auto">
                     <h1 className=" font-bold"><T>Get to</T> know me</h1>
-                    <div className="inline">
-                        <img src={Laravel} width={20} className="inline"/>
-                        <img src={Bootstrap} width={20} className="inline"/>
-                    </div>
+                    <Logos logos={[{logo: Laravel, width:20},{logo: Bootstrap, width:20}]}/>
                     <p>I started learning Laravel and Bootstrap since 2021 and have built some applications with it. Tracker, youRnoteS, MenITi, To-Gather (link)</p>
-                    <div className="inline">
-                        <img src={Tailwind} width={20} className="inline"/>
-                        <img src={React} width={20} className="inline"/>
-                    </div>
+                    <Logos logos={[{logo: Tailwind, width:20},{logo: React, width:20}]}/>
                     <p>I’ve recently been interested built front-end web using Tailwind and React. (Tracker, youRnoteS)</p>
-                    <img src={Threejs} width={20}/>
+                    <Logos logos={[{logo: Threejs, width:20}]}/>
                     <p>I recently fell in love with learning Three.js. I implemented Three.js as well as React and Tailwind on this portfolio based on what I've learned so far.</p>
-                    <div className="inline">
-                        <img src={PostgreSQL} width={23} className="inline"/>
-                        <img src={phpMyAdmin} width={30} className="inline"/>
-                    </div>
+                    <Logos logos={[{logo: PostgreSQL, width:23},{logo: phpMyAdmin, width:30}]}/>
                     <p>I use PostgreSQL and phpMyAdmin for database management, i prefer PostgreSQL for interface, familiarity, performance and scalability.</p>
-                    <div className="inline">
-                        <img src={JavaScript} width={25} className="inline"/>
-                        <img src={PHP} width={25} className="inline"/>
-                        <img src={Figma} width={20} className="inline"/>
-                        <img src={AdminLTE} width={20} className="inline"/>
-                        <img src={HTML} width={25} className="inline"/>
-                        <img src={CSS} width={25} className="inline"/>
-                    </div>
+                    <Logos logos={[{logo: JavaScript, width:25},{logo: PHP, width:25}, {logo: Figma, width:20},{logo: AdminLTE, width:20}, {logo: HTML, width:25},{logo: CSS, width:25}]}/>
                     <p>Those are languages, frameworks, and tools that I’m familiar, confident, and have worked with.</p>
                 </div>
             </div>
