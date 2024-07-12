b26018f2a9c0e58471096a38461253981dad0cd6
millahh <millahmillah37@gmail.com>
2024-07-12 23:04:18 +0700

    about section

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+import AdminLTE from "./tech/adminlte.png";
+import Bootstrap from "./tech/bootstrap.png";
+import CSS from "./tech/css.png";
+import Figma from "./tech/figma.png";
+import HTML from "./tech/html.png";
+import JavaScript from "./tech/javascript.png";
+import Laravel from "./tech/laravel.png";
+import PHP from "./tech/php.png";
+import phpMyAdmin from "./tech/phpmyadmin.png";
+import PostgreSQL from "./tech/postgresql.png";
+import React from "./tech/reactjs.png";
+import Tailwind from "./tech/tailwind.png";
+import Threejs from "./tech/threejs.png";
+import Github from "./github.png";
+
+export { AdminLTE, Bootstrap, CSS, Figma, HTML, JavaScript, Laravel, PHP, phpMyAdmin, PostgreSQL, React, Tailwind, Threejs, Github };
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,9 @@
 import T from "./texts/Title";
+import { AdminLTE, Bootstrap, CSS, Figma, HTML, JavaScript, Laravel, PHP, phpMyAdmin, PostgreSQL, React, Tailwind, Threejs, Github } from "../assets";
 import { motion } from "framer-motion";
-import { Float } from "@react-three/drei";
 
 const HardSkills = () => {
     const animation1 = {
-        whileHover:{
-            scale: 1.2,
-            transition: { duration: 1 },
-        },
         visible:{
             x: [0, 3, 0],  
             y: [0, -3, 0],  
@@ -20,10 +16,6 @@ const HardSkills = () => {
         }
     }
     const animation2 = {
-        whileHover:{
-            scale: 1.2,
-            transition: { duration: 1 },
-        },
         visible:{
             x: [0, -3, 0],  
             y: [0, -3, 0],  
@@ -37,50 +29,57 @@ const HardSkills = () => {
     }
     return(
         <motion.div className="hard-skills cursor-pointer" animate="visible">
-            <motion.p className=" top-20 left-52 font-bold text-2xl" variants={animation2} whileHover="whileHover">Bootstrap</motion.p> 
-            <motion.p className=" top-36 left-32" variants={animation1} whileHover="whileHover">AdminLTE</motion.p>
-            <motion.p className=" top-36 left-96" variants={animation1} whileHover="whileHover">CSS</motion.p>
-            <motion.p className=" top-44 left-60" variants={animation2} whileHover="whileHover">HTML</motion.p>
-            <motion.p className=" top-56 left-24" variants={animation2} whileHover="whileHover">Figma</motion.p>
-            <motion.p className=" top-56 left-80 text-lg text-primary font-semibold" variants={animation2} whileHover="whileHover">JavaScript</motion.p>
-            <motion.p className=" top-64 left-52" variants={animation2} whileHover="whileHover">Three.js</motion.p>
-            <motion.p className=" top-72 left-18 text-lg text-primary font-semibold" variants={animation1} whileHover="whileHover">phpMyAdmin</motion.p>
-            <motion.p className=" bottom-60 left-96 font-bold text-2xl" variants={animation2} whileHover="whileHover">Tailwind</motion.p>
-            <motion.p className=" bottom-44 left-40 font-bold text-2xl" variants={animation1} whileHover="whileHover">Laravel</motion.p>
-            <motion.p className=" bottom-44 left-80" variants={animation2} whileHover="whileHover">UI/UX</motion.p>
-            <motion.p className=" top-96 left-96 text-lg text-primary font-semibold" variants={animation2} whileHover="whileHover">React</motion.p>
-            <motion.p className=" bottom-28 left-28 text-lg text-primary font-semibold" variants={animation2} whileHover="whileHover">PHP</motion.p>
-            <motion.p className=" bottom-16 left-52 font-bold text-2xl" variants={animation1} whileHover="whileHover">PostgreSQL</motion.p>
-
-            {/* <p>HTML</p>
-            <p>Bootstrap</p>
-            <p>CSS</p>
-            <p>phpMyAdmin</p>
-            <p>JavaScript</p>
-            <p>UI/UX</p>
-            <p>Figma</p>
-            <p>Laravel</p>
-            <p>Tailwind</p>
-            <p>Three.js</p>
-            <p>PHP</p>
-            <p>React</p>
-            <p>PostgreSQL</p> */}
+            <motion.p className=" top-20 left-52 font-bold text-2xl" variants={animation2}>Bootstrap</motion.p> 
+            <motion.p className=" top-36 left-32" variants={animation1}>AdminLTE</motion.p>
+            <motion.p className=" top-36 left-96" variants={animation1}>CSS</motion.p>
+            <motion.p className=" top-44 left-60" variants={animation2}>HTML</motion.p>
+            <motion.p className=" top-56 left-24" variants={animation2}>Figma</motion.p>
+            <motion.p className=" top-56 left-80 text-lg text-primary font-semibold" variants={animation2}>JavaScript</motion.p>
+            <motion.p className=" top-64 left-52" variants={animation2}>Three.js</motion.p>
+            <motion.p className=" top-72 left-18 text-lg text-primary font-semibold" variants={animation1}>phpMyAdmin</motion.p>
+            <motion.p className=" bottom-60 left-96 font-bold text-2xl" variants={animation2}>Tailwind</motion.p>
+            <motion.p className=" bottom-44 left-40 font-bold text-2xl" variants={animation1}>Laravel</motion.p>
+            <motion.p className=" bottom-44 left-80" variants={animation2}>UI/UX</motion.p>
+            <motion.p className=" top-96 left-96 text-lg text-primary font-semibold" variants={animation2}>React</motion.p>
+            <motion.p className=" bottom-28 left-28 text-lg text-primary font-semibold" variants={animation2}>PHP</motion.p>
+            <motion.p className=" bottom-16 left-52 font-bold text-2xl" variants={animation1}>PostgreSQL</motion.p>
         </motion.div>
     )
 }
 
 const About = () => {
     return(
-        <div className="container px-10 min-w-full">
+        <div className="container pl-10 min-w-full h-full">
             <HardSkills/>
             <div className="flex flex-row">
-                <div className="basis-1/2"></div>
-                <div className="basis-1/2 skills">
-                    <h1><T>Get to</T> know me</h1>
+                <div className=" basis-2/5"></div>
+                <div className=" basis-3/5 py-10 px-10 h-screen overflow-y-auto">
+                    <h1 className=" font-bold"><T>Get to</T> know me</h1>
+                    <div className="inline">
+                        <img src={Laravel} width={20} className="inline"/>
+                        <img src={Bootstrap} width={20} className="inline"/>
+                    </div>
                     <p>I started learning Laravel and Bootstrap since 2021 and have built some applications with it. Tracker, youRnoteS, MenITi, To-Gather (link)</p>
+                    <div className="inline">
+                        <img src={Tailwind} width={20} className="inline"/>
+                        <img src={React} width={20} className="inline"/>
+                    </div>
                     <p>I’ve recently been interested built front-end web using Tailwind and React. (Tracker, youRnoteS)</p>
+                    <img src={Threejs} width={20}/>
                     <p>I recently fell in love with learning Three.js. I implemented Three.js as well as React and Tailwind on this portfolio based on what I've learned so far.</p>
+                    <div className="inline">
+                        <img src={PostgreSQL} width={23} className="inline"/>
+                        <img src={phpMyAdmin} width={30} className="inline"/>
+                    </div>
                     <p>I use PostgreSQL and phpMyAdmin for database management, i prefer PostgreSQL for interface, familiarity, performance and scalability.</p>
+                    <div className="inline">
+                        <img src={JavaScript} width={25} className="inline"/>
+                        <img src={PHP} width={25} className="inline"/>
+                        <img src={Figma} width={20} className="inline"/>
+                        <img src={AdminLTE} width={20} className="inline"/>
+                        <img src={HTML} width={25} className="inline"/>
+                        <img src={CSS} width={25} className="inline"/>
+                    </div>
                     <p>Those are languages, frameworks, and tools that I’m familiar, confident, and have worked with.</p>
                 </div>
             </div>
