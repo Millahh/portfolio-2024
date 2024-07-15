3f3d856396c02f4a7dfa5a188888082b83270d42
millahh <millahmillah37@gmail.com>
2024-07-15 23:30:51 +0700

    add objects

--- src/components/canvas/Hobbies.jsx
+++ src/components/canvas/Hobbies.jsx
@@ -8,44 +8,36 @@ import { Box, OrbitControls} from "@react-three/drei";
 import CanvasLoader from '../LoaderProgressBar';
 import { useGLTF } from "@react-three/drei";
 
-const Music = ({ position, rotation }) => {
-    const music = useGLTF('./music.glb')
-
+const Objects = ({ position, rotation, scale, object, light }) => {
     return (
-            <Canvas 
-                frameloop="always"
-                shadows
-                // camera={{ position: [200, 10, 50], fov: 1}}
-                gl={{ preserveDrawingBuffer: true}}
-                >
-                <Suspense fallback={<CanvasLoader all/>}>
-                        <OrbitControls enableZoom={true} enableRotate={true}/>
-                        <directionalLight position={[300, 300, 200]} intensity={6}/>
-                        <mesh position={position}>
-                            <primitive 
-                                object={music.scene} 
-                                scale={25} 
-                                position={[0, 0, 0]} 
-                                rotation={rotation}
-                                /> 
-                        </mesh>
-                </Suspense>
-            </Canvas>
+        <>
+            <OrbitControls enableZoom={false} enableRotate={true}/>
+            <directionalLight position={light} intensity={3}/>
+            <mesh position={position}>
+                <primitive 
+                    object={object.scene} 
+                    scale={scale} 
+                    position={[0, 0, 0]} 
+                    rotation={rotation}
+                    /> 
+            </mesh>
+        </>
     )
 }
 
-const Mic = ({ position, rotation }) => {
-    const mic = useGLTF('./mic.glb')
-
+const CanvasObjects = () => {
     return (
-        <mesh position={position}>
-            <primitive 
-                object={mic.scene} 
-                scale={0.001} 
-                position={[0, 0, 0]} 
-                rotation={rotation}
-                /> 
-        </mesh>
+        <Canvas 
+        frameloop="always"
+        shadows
+        gl={{ preserveDrawingBuffer: true}}
+        >
+        <Suspense fallback={<CanvasLoader all/>}>
+            <Objects position={[-6.5, 2.2, -0.2]} rotation={[0, 0, -0.2]} scale={32} light={[-0.1, 0, 0.07]} object={useGLTF('./music.glb')}/>
+            <Objects position={[-4.5, -2.5, 0.3]} rotation={[0.1, 0.2, -0.5]} scale={0.0013} light={[0.9, -0.3, 0.5]} object={useGLTF('./mic.glb')}/>
+            <Objects position={[6, -2.7, -5]} rotation={[7, 5.1, -12]} scale={1.4} light={[0.9, -0.3, 0.5]} object={useGLTF('./coffee.glb')}/>
+        </Suspense>
+    </Canvas>
     )
 }
 
@@ -53,12 +45,12 @@ const Hobbies = () => {
     return(
         <>
             <div className="h-screen w-screen absolute my-auto -z-2">
-                <Music position={[-5, 1.5, 1.2]} rotation={[0, 0.4, -0.16]}/>
+                <CanvasObjects />
             </div>
             <div className="container text-center px-48 h-screen min-w-full content-center z-99">
-                <div className=" align-middle">
+                <div className="align-middle">
                     <h1 className="font-bold mb-2"><T>Fun</T> facts</h1>
-                    <p>I sip <P>coffee</P> whenever my <P>code</P> doesn’t work well and sometimes go for a little <P>walk</P>. <br></br>I like to <P>dance</P> when im bored. I love <P>singing</P> (i literally singing <P>everyday</P>), sometimes i <P>recorded</P> it and <P>posted</P> it on my Insta. I hate <P>washing dishes</P> (<P>i do</P> wash my dishes everyday). My favorite color is <P>yellow</P>, <P>not purple</P>. And i hate <P>house lizard</P> more than <P>cockroach</P>.</p>
+                    <p>I sip <P>coffee</P> whenever my <P>code</P> doesn’t work well and sometimes go for a little <P>walk</P>. <br></br>I like to <P>dance</P> when im bored. I love <P>singing</P> (i literally singing <P>everyday</P>), sometimes i <P>recorded</P> it and <P>posted</P> it on my Insta. I hate <P>washing dishes</P> (<P>i do</P> wash my dishes everyday). My favorite color is <P>yellow</P>, <P>not purple</P>. Last, i hate <P>house lizard</P> more than <P>cockroach</P>.</p>
                 </div>
             </div>
         </>
