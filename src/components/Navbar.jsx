18d647062a5edef553fdd77a1615859066373bb3
millahh <millahmillah37@gmail.com>
2024-07-19 03:42:43 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@
 const Navbar = () => {
     return (
         <>
-            <p className="text-primary inline font-bold"> </p>
+            <div className="navbar flex justify-between px-10 py-5 max-md:w-full w-1/2">
+                <a className="flex hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></a>
+                <a>ABOUT</a>
+                <a>PROJECTS</a>
+                <a>EXPERIENCES</a>
+                <a>FUN FACTS</a>
+                <a>CONTACT</a>
+            </div>
         </>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,14 +2,16 @@
 import { motion, AnimatePresence } from "framer-motion";
 import P from "./texts/Paragraph";
 import { Github } from "../assets";
+import Navbar from "./Navbar";
 
 const Projects = () => {
 
     return (
         <>
+            <Navbar/>
             <div className="projects py-10 px-20 min-w-full h-screen overflow-y-hidden flex flex-row">
                 <div className=" basis-4/12 space-y-10 overflow-y-auto items-center">
-                    <div className="relative w-4/5 h-2/5 self-center mx-auto">
+                    <div className="relative w-4/5 h-2/5 self-center mx-auto hover:scale-110 transition-all duration-150">
                         <img src="./experiences/image.png" className=" object-cover w-full h-full border-4 rounded-xl absolute z-0 desc"/>
                         <div className=" absolute z-1 bg-black bg-opacity-50 w-full h-full border-4 rounded-xl p-3 desc-hide">
                             <div className="github bg-tertiary self-center px-2 text-md rounded-2xl align-middle inline-block">
