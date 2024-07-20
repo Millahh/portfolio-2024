f503626d2af8acd250268100979226d2a814cd29
millahh <millahmillah37@gmail.com>
2024-07-21 03:18:06 +0700

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <>
-            <div className="navbar flex md:justify-between px-10 py-5 w-1/2">
+            <div className="navbar flex md:justify-between px-10 py-5 xl:w-1/2 lg:w-2/3 md:w-full max-md:hidden max-sm:hidden max-xs:hidden">
                 <a className="flex hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></a>
                 <a>ABOUT</a>
                 <a>PROJECTS</a>
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <a>FUN FACTS</a>
                 <a>CONTACT</a>
             </div>
-            <div class="max-w-screen-xl pt-4 pr-4">
+            <div class="md:hidden max-w-screen-xl pt-4 pr-4">
                 <button type="button" class="float-right p-2 w-8 h-8 rounded-lg bg-primary bg-opacity-30 hover:bg-opacity-40 focus:outline-none hover:border-transparent" onClick={() => setIsOpen(!isOpen)}>
                     <svg class="w-4 h-4 float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M1 1h15M1 7h15M1 13h15"/>
