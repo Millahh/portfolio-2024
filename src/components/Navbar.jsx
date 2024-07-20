4e44bc8e63829e95f92a2aa72389ee005fc56c1c
millahh <millahmillah37@gmail.com>
2024-07-20 23:44:29 +0700

    experiences -edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,15 @@
 const Navbar = () => {
     return (
         <>
-            <div className="navbar flex justify-between px-10 py-5 max-md:w-full w-1/2">
+            {/* <div className="navbar flex md:justify-between px-10 py-5 max-md:w-full w-1/2">
+                <a className="flex hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></a>
+                <a>ABOUT</a>
+                <a>PROJECTS</a>
+                <a>EXPERIENCES</a>
+                <a>FUN FACTS</a>
+                <a>CONTACT</a>
+            </div> */}
+            <div className="navbar flex md:justify-between px-10 py-5 max-md:w-full w-1/2">
                 <a className="flex hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></a>
                 <a>ABOUT</a>
                 <a>PROJECTS</a>
