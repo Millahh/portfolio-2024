bf9a65434e830c38cf83ee8ea1bb882ed3a3ad83
millahh <millahmillah37@gmail.com>
2024-07-23 00:05:28 +0700

    custom arrows

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import P from "./texts/Paragraph";
 import { Github } from "../assets";
 import Navbar from "./Navbar";
 import { useState, useEffect } from "react";
-import { Carousel } from "@material-tailwind/react";
+import { Carousel, IconButton } from "@material-tailwind/react";
 
 const Photos = () => {
     return (
@@ -134,7 +134,7 @@ const Projects = () => {
                     </div>
                 </div>
             }{isMobile && 
-                <Carousel
+                <Carousel className=" h-1/3"
                     navigation={({ setActiveIndex, activeIndex, length }) => (
                         <div className="absolute top-52 left-2/4 z-50 flex -translate-x-2/4 gap-2">
                         {new Array(length).fill("").map((_, i) => (
@@ -147,6 +147,54 @@ const Projects = () => {
                             />
                         ))}
                         </div>
+                    )}
+                    prevArrow={({ handlePrev }) => (
+                        <IconButton
+                        variant="text"
+                        color="white"
+                        size="lg"
+                        onClick={handlePrev}
+                        className="!absolute top-2/4 left-4 -translate-y-2/4"
+                        >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="h-6 w-6"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                            />
+                        </svg>
+                        </IconButton>
+                    )}
+                    nextArrow={({ handleNext }) => (
+                        <IconButton
+                        variant="text"
+                        color="white"
+                        size="lg"
+                        onClick={handleNext}
+                        className="!absolute top-2/4 !right-4 -translate-y-2/4"
+                        >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="h-6 w-6"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                            />
+                        </svg>
+                        </IconButton>
                     )}>
                 <div
                         onFocus={() => {
