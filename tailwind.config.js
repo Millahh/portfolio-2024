cb2c9ca89b82a114e312dcb52c5c97d8c688d4f2
millahh <millahmillah37@gmail.com>
2024-07-22 23:00:03 +0700

    add @material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+
   ],
   mode: "jit",
   theme: {
