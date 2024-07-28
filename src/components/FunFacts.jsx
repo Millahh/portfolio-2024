c6e3e543c0574e2d02b56f5bff63a8f09f362fe0
millahh <millahmillah37@gmail.com>
2024-07-29 06:10:52 +0700

    responsive funfact

--- src/components/FunFacts.jsx
+++ src/components/FunFacts.jsx
@@ -10,10 +10,10 @@ const FunFacts = () => {
     return(
         <>
             <Hobbies />
-            <div className="container text-center px-48 h-screen min-w-full content-center z-99">
+            <div className="container text-center px-48 max-[850px]:px-20 h-screen m-auto content-center z-99">
                 <div className="align-middle">
-                    <h1 className="font-bold mb-2"><T>Fun</T> facts</h1>
-                    <p>I sip <P>coffee</P> whenever my <P>code</P> doesn’t work well and sometimes go for a little <P>walk</P>. <br></br>I like to <P>dance</P> when im bored. I love <P>singing</P> (i literally singing <P>everyday</P>), sometimes i <P>recorded</P> it and <P>posted</P> it on my Insta. I hate <P>washing dishes</P> (<P>i do</P> wash my dishes everyday). My favorite color is <P>yellow</P>, <P>not purple</P>. Last, i hate <P>house lizard</P> more than <P>cockroach</P>.</p>
+                    <p className="font-bold mb-2 max-[850px]:text-xl"><T>Fun</T> facts</p>
+                    <p className=" max-[850px]:text-sm">I sip <P>coffee</P> whenever my <P>code</P> doesn’t work well and sometimes go for a little <P>walk</P>. <br></br>I like to <P>dance</P> when im bored. I love <P>singing</P> (i literally singing <P>everyday</P>), sometimes i <P>recorded</P> it and <P>posted</P> it on my Insta. I hate <P>washing dishes</P> (<P>i do</P> wash my dishes everyday). My favorite color is <P>yellow</P>, <P>not purple</P>. Last, i hate <P>house lizard</P> more than <P>cockroach</P>.</p>
                 </div>
             </div>
         </>
--- src/components/canvas/Hobbies.jsx
+++ src/components/canvas/Hobbies.jsx
@@ -25,23 +25,40 @@ const Objects = ({ position, rotation, scale, object, light }) => {
     )
 }
 
-const Hobbies = () => {
+const CanvasHobbies = () => {
     return (
         <>
-            <div className="h-screen w-screen absolute my-auto -z-2">
-                <Canvas 
-                frameloop="always"
-                shadows
-                gl={{ preserveDrawingBuffer: true}}
-                >
-                    <Suspense fallback={<CanvasLoader all/>}>
-                        <Objects position={[-6.5, 2.2, -0.2]} rotation={[0, 0, -0.2]} scale={35} light={[0, 0, 0.1]} object={useGLTF('./objects/music.glb')}/>
-                        <Objects position={[-4.2, -2.8, 0.2]} rotation={[0.1, 0.2, -0.5]} scale={0.0013} light={[0.5, -0.5, 0]} object={useGLTF('./objects/mic.glb')}/>
-                        <Objects position={[6.5, -4.5, -3]} rotation={[7, 11.4, -11]} scale={2} light={[0, 50, 0]} object={useGLTF('./objects/coffee.glb')}/>
-                        <Objects position={[8, 2.3, -2.5]} rotation={[5, 3.4, -12]} scale={0.11} light={[100, 100, 10]} object={useGLTF('./objects/dance.glb')}/>
-                    </Suspense>
-                </Canvas>
+            <Canvas 
+            frameloop="always"
+            shadows
+            gl={{ preserveDrawingBuffer: true}}
+            >
+                <Suspense fallback={<CanvasLoader all/>}>
+                    <Objects position={[-6.5, 2.2, -0.2]} rotation={[0, 0, -0.2]} scale={35} light={[0, 0, 0.1]} object={useGLTF('./objects/music.glb')}/>
+                    <Objects position={[-4.2, -2.8, 0.2]} rotation={[0.1, 0.2, -0.5]} scale={0.0013} light={[0.5, -0.5, 0]} object={useGLTF('./objects/mic.glb')}/>
+                    <Objects position={[6.5, -4.5, -3]} rotation={[7, 11.4, -11]} scale={2} light={[0, 50, 0]} object={useGLTF('./objects/coffee.glb')}/>
+                    <Objects position={[8, 2.3, -2.5]} rotation={[5, 3.4, -12]} scale={0.11} light={[100, 100, 10]} object={useGLTF('./objects/dance.glb')}/>
+                </Suspense>
+            </Canvas>
+        </>
+    )
+}
+
+const Hobbies = () => {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    const isMobile = h>w ? true : false;
+    return (
+        <>
+            {isMobile && 
+            <div className="h-1/2 w-full max-[850px]:h-1/3 absolute top-1/4 max-[850px]:top-1/3 -z-2">
+                <CanvasHobbies/>
             </div>
+            }{!isMobile &&
+            <div className="h-screen w-screen absolute m-auto -z-2">
+                <CanvasHobbies/>
+            </div>
+            }
         </>
     )
 }
