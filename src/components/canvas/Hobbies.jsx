15554e01af26a71bcab20cfa21aba2aaccd6ae23
millahh <millahmillah37@gmail.com>
2024-07-31 18:05:52 +0700

    loader bug fixer

--- src/components/canvas/Hobbies.jsx
+++ src/components/canvas/Hobbies.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
-import { Suspense, useEffect } from 'react';
+import { useState, useRef, Suspense, useEffect } from 'react';
 import { Canvas } from "@react-three/fiber";
 import { Float, OrbitControls} from "@react-three/drei";
 import CanvasLoader from '../LoaderProgressBar';
 import { useGLTF } from "@react-three/drei";
-import { useRef, useState } from 'react'
 
 const Objects = ({ position, rotation, scale, object, light }) => {
+    const assets = [useGLTF('./objects/music.glb'), useGLTF('./objects/mic.glb'), useGLTF('./objects/coffee.glb'), useGLTF('./objects/dance.glb')]
     const ref = useRef()
     const [isHovered, setIsHovered] = useState(false);
     useEffect(() => {
@@ -20,7 +20,7 @@ const Objects = ({ position, rotation, scale, object, light }) => {
                 <directionalLight position={light} intensity={2}/>
                 <mesh position={position}>
                     <primitive 
-                        object={object.scene} 
+                        object={assets[object].scene} 
                         scale={scale} 
                         position={[0, 0, 0]} 
                         rotation={rotation}
@@ -51,10 +51,10 @@ const Hobbies = () => {
                 gl={{ preserveDrawingBuffer: true}}
                 >
                     <Suspense fallback={<CanvasLoader all/>}>
-                        <Objects position={isMobile?[-2, 3, 0]:[-5.8, 2.2, -0.2]} rotation={[0, 0, -0.2]} scale={isMobile?20:35} light={[0, 0, 0.1]} object={useGLTF('./objects/music.glb')}/>
-                        <Objects position={isMobile?[-2, -3, 0]:[-4.2, -2.8, 0.2]} rotation={[0.1, 0.2, -0.5]} scale={0.0013} light={[0.5, -0.5, 0]} object={useGLTF('./objects/mic.glb')}/>
-                        <Objects position={isMobile?[2, -2.95, 0.5]:[6.5, -4.5, -3]} rotation={[7, 11.4, -11]} scale={isMobile?0.7:2} light={[0, 50, 0]} object={useGLTF('./objects/coffee.glb')}/>
-                        <Objects position={isMobile?[4, 4.2, -4.7]:[8, 2.3, -2.5]} rotation={isMobile?[5, 3.6, -12.2]:[5, 3.4, -12]} scale={isMobile?0.085:0.11} light={[100, 100, 10]} object={useGLTF('./objects/dance.glb')}/>
+                        <Objects position={isMobile?[-2, 3, 0]:[-5.8, 2.2, -0.2]} rotation={[0, 0, -0.2]} scale={isMobile?20:35} light={[0, 0, 0.1]} object={0}/>
+                        <Objects position={isMobile?[-2, -3, 0]:[-4.2, -2.8, 0.2]} rotation={[0.1, 0.2, -0.5]} scale={0.0013} light={[0.5, -0.5, 0]} object={1}/>
+                        <Objects position={isMobile?[2, -2.95, 0.5]:[6.5, -4.5, -3]} rotation={[7, 11.4, -11]} scale={isMobile?0.7:2} light={[0, 50, 0]} object={2}/>
+                        <Objects position={isMobile?[4, 4.2, -4.7]:[8, 2.3, -2.5]} rotation={isMobile?[5, 3.6, -12.2]:[5, 3.4, -12]} scale={isMobile?0.085:0.11} light={[100, 100, 10]} object={3}/>
                     </Suspense>
                 </Canvas>
             </div>
