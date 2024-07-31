7f6561476aa6254d3e9e3213b33e9a4e41e24775
millahh <millahmillah37@gmail.com>
2024-08-01 01:28:05 +0700

    add style to navlink when active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,20 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import { navLinks } from "../constants";
+import { NavLink } from 'react-router-dom';
 import { Outlet, Link } from "react-router-dom";
+import { color } from "framer-motion";
 
-const Links = () => {
+const MyLinks = () => {
     return (
         <>
             {navLinks.map((link) => (
-                <Link to={link.id} 
+                <NavLink 
+                activeClassName="active"
+                to={link.id} 
                 key={link.id}
-                className="max-md:flex m-auto max-md:mr-0 justify-center max-md:justify-end hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100 focus:opacity-100 max-md:w-20"
-                >{link.title}</Link>
+                className="max-md:flex m-auto max-md:mr-0 justify-center max-md:justify-end hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100 focus:text-white max-md:w-20"
+                >{link.title}</NavLink>
             ))}
         </>
     )
@@ -22,8 +26,8 @@ const Navbar = () => {
     return (
         <>
             <div className="navbar flex md:justify-between px-10 pt-5 xl:w-1/2 lg:w-2/3 md:w-full 2xl:m-auto max-md:hidden max-sm:hidden max-xs:hidden">
-                <Link to="/" className="flex hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></Link>
-                <Links/>
+                <NavLink activeClassName="active" to="/" className="flex hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></NavLink>
+                <MyLinks/>
             </div>
             <div class="md:hidden max-w-screen-xl pt-4 pr-4">
                 <button type="button" class="float-right p-2 w-8 h-8 rounded-lg bg-primary bg-opacity-30 hover:bg-opacity-40 focus:outline-none hover:border-transparent" onClick={() => setIsOpen(!isOpen)}>
@@ -33,8 +37,8 @@ const Navbar = () => {
                 </button>
                 {isOpen && 
                 <div class="navbar w-1/2 max-md:w-1/3 max-md:bg-primary max-md:bg-opacity-10 text-center self-center m-auto max-md:mr-0 pl-4 max-md:pr-10" id="navbar-mobile">
-                    <Link to="/" className="flex m-auto hover:scale-125 justify-center max-md:mr-0 max-md:justify-end transition-transform duration-100 opacity-50 hover:opacity-100 w-20">HI<p className="text-sm -ml-1">👋</p></Link>
-                    <Links/>
+                    <NavLink activeClassName="active" to="/" className="flex m-auto hover:scale-125 justify-center max-md:mr-0 max-md:justify-end transition-transform duration-100 opacity-50 hover:opacity-100 w-20">HI<p className="text-sm -ml-1">👋</p></NavLink>
+                    <MyLinks/>
                 </div>
                 }
             </div>
