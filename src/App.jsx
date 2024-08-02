55aa636dc1c42474e41b82027a6789f4a385ecc9
millahh <millahmillah37@gmail.com>
2024-08-03 00:17:20 +0700

    add transition

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Intro, AutoLoad, About, FunFacts, Experiences, Projects } from "./components";
 import { DiceIntro, Hobbies } from "./components/canvas";
 import { AnimCursor } from "./constants";
-// import { useState, useEffect } from "react";
 import { navLinks } from "./constants";
 import { NavLink } from 'react-router-dom';
-import { Outlet, Link } from "react-router-dom";
-import { color } from "framer-motion";
 
 function getPageComponent(page) {
   switch (page) {
@@ -33,22 +30,20 @@ function getPageComponent(page) {
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  const [isCurrentItemActive, setIsCurrentItemActive] = useState('inactive');
   const [currentPage, setCurrentPage] = useState('home');
   return (
     <BrowserRouter>
       <AnimCursor/>
       <DiceIntro/>
-      <div className="w-full h-12 bg-black bg-opacity-50 absolute z-20 max-md:hidden">
+      <div className="w-full h-12 bg-black bg-opacity-40 absolute z-20 max-md:hidden">
         <div className="navbar flex md:justify-between px-10 pt-5 xl:w-1/2 lg:w-2/3 md:w-full 2xl:m-auto max-md:hidden max-sm:hidden max-xs:hidden absolute z-20">
-          <NavLink activeclassname={({ isActive }) => (isActive ? 'active' : 'inactive') (setIsCurrentItemActive('active'))} onClick={() => setCurrentPage('home')} to="/" className="flex hover:scale-125 max-md:mr-0 max-md:justify-end transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></NavLink>
+          <NavLink  onClick={() => setCurrentPage('home')} to="/" className={`${currentPage === 'home' ? 'upScalling' : ''} flex hover:scale-125 max-md:mr-0 max-md:justify-end transition-all duration-100 text-gray-500 hover:text-white`}>HI<p className="text-sm -ml-1">👋</p></NavLink>
             {navLinks.map((link) => (
                 <NavLink 
-                activeclassname={({ isActive }) => (isActive ? 'active' : 'inactive') (setIsCurrentItemActive('active'))}
                 onClick={() => setCurrentPage(link.id)}
                 to="/" 
                 key={link.id}
-                className="max-md:flex m-auto max-md:mr-0 justify-center max-md:justify-end hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100 focus:text-white max-md:w-20"
+                className={`${currentPage === link.id ? 'upScalling' : ''} max-md:flex m-auto max-md:mr-0 justify-center max-md:justify-end hover:scale-125 transition-all duration-100 text-gray-500 hover:text-white max-md:w-20`}
                 >{link.title}</NavLink>
             ))}
         </div>
@@ -61,20 +56,19 @@ function App() {
           </button>
           {isOpen && 
           <div class="navbar w-1/2 max-md:w-1/3 max-md:bg-primary max-md:bg-opacity-10 text-center self-center m-auto max-md:mr-0 pl-4 max-md:pr-12" id="navbar-mobile">
-            <NavLink activeclassname={({ isActive }) => (isActive ? 'active' : 'inactive') (setIsCurrentItemActive('active'))} onClick={() => setCurrentPage('home')} to="/" className="flex hover:scale-125 max-md:mr-0 max-md:justify-end transition-transform duration-100 opacity-50 hover:opacity-100">HI<p className="text-sm -ml-1">👋</p></NavLink>
+            <NavLink  onClick={() => setCurrentPage('home')} to="/" className={`${currentPage === 'home' ? 'upScalling' : ''} flex hover:scale-125 max-md:mr-0 max-md:justify-end transition-transform duration-100 opacity-50 hover:opacity-100`}>HI<p className="text-sm -ml-1">👋</p></NavLink>
               {navLinks.map((link) => (
                   <NavLink 
-                  activeclassname={({ isActive }) => (isActive ? 'active' : 'inactive') (setIsCurrentItemActive('active'))}
                   onClick={() => setCurrentPage(link.id)}
                   to="/" 
                   key={link.id}
-                  className="max-md:flex m-auto max-md:mr-0 justify-center max-md:justify-end hover:scale-125 transition-transform duration-100 opacity-50 hover:opacity-100 focus:text-white max-md:w-20"
+                  className={`${currentPage === link.id ? 'upScalling' : ''} max-md:flex m-auto max-md:mr-0 justify-center max-md:justify-end hover:scale-125 transition-transform duration-100 text-gray-500 hover:text-white max-md:w-20`}
                   >{link.title}</NavLink>
               ))}
           </div>
           }
       </div>
-      <Routes path="/">
+      <Routes>
       <Route path="/" element={getPageComponent(currentPage)} />
       </Routes>
     </BrowserRouter>
